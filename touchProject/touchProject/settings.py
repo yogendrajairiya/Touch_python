4c81dc2190a5799546b28180e915c94fe87ab8aa
Yogendra Jairiya <yogendrajairiya01@gmail.com>
2025-06-27 17:43:11 +0530

    Added touch list page and media cofig

--- touchProject/touchProject/settings.py
+++ touchProject/touchProject/settings.py
@@ -128,6 +128,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+MEDIA_URL = '/media/' # URL to access media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Directory to store uploaded media files
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
