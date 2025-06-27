dd739719537615d7b421730957ded66a50cc50a2
Yogendra Jairiya <yogendrajairiya01@gmail.com>
2025-06-27 17:45:17 +0530

    Added view and full details page

--- touchProject/touch/urls.py
+++ touchProject/touch/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_touch, name='all_touch'),
+    path('<int:touch_id>/', views.touch_detail, name='touch_detail'),
 ]
--- touchProject/touch/views.py
+++ touchProject/touch/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-
+from .models import TouchVariety
+from django.shortcuts import get_object_or_404
 # Create your views here.
 def all_touch(request):
-    return render(request, 'touch/all_touch.html')
+    touches = TouchVariety.objects.all()
+    return render(request, 'touch/all_touch.html', {'touches': touches})
+
+def touch_detail(request, touch_id):
+    touch = get_object_or_404(TouchVariety, id=touch_id)
+    
+    return render(request, 'touch/touch_detail.html', {'touch': touch})
