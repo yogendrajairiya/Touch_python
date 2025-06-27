2089412325fe7dd8344dafcc16a42dff4297e0b3
Yogendra Jairiya <yogendrajairiya01@gmail.com>
2025-06-27 17:37:31 +0530

    Added touch model with admin

--- touchProject/touch/models.py
+++ touchProject/touch/models.py
@@ -3,24 +3,20 @@ from django.utils import timezone
 # Create your models here.
 class TouchVariety(models.Model):
     TOUCH_CHOICES = [
-        ('Haptic', 'Haptic'),
-        ('Visual', 'Visual'),
-        ('Auditory', 'Auditory'),
-        ('Olfactory', 'Olfactory'),
-        ('Gustatory', 'Gustatory'),
+        ('HA', 'Haptic'),
+        ('VI', 'Visual'),
+        ('AU', 'Auditory'),
+        ('OL', 'Olfactory'),
+        ('GU', 'Gustatory'),
     ]
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='touch_images/', blank=True, null=True)
     description = models.TextField()
-    type = models.CharField(max_length=50, choices=TOUCH_CHOICES, default='Haptic')
+    type = models.CharField(max_length=50, choices=TOUCH_CHOICES)
     date_added = models.DateTimeField(default=timezone.now)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = 'Touch Variety'
-        verbose_name_plural = 'Touch Variety'
-        ordering = ['-created_at']  # Order by creation date, newest first
+def __str__(self):
+    return self.name
